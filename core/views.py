4f48b9071b2478c149780817a7562c83754d3f18
Aizada <kasenova.aizada@gmail.com>
2020-11-11 20:17:02 +0600

    inir python project

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
+def homepage(request):
+    return render(request, "core/index.html")
+
+def authorization(request):
+    if request.method == 'POST':
+        form = request.POST
+        username = form["login"]
+        password = form["password"]
+        user = User.objects.get(username=username)
+        if user.check_password(password):
+            login(request, user)
+            return redirect(homepage)
+
+    return render(request, "core/login.html") 
