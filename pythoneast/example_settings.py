64cd9baab6cdddb93ec5233159f55128a056cd93
Aizada <kasenova.aizada@gmail.com>
2020-11-12 18:09:32 +0600

    added readme and requirements, removed settings.py

--- pythoneast/example_settings.py
+++ pythoneast/example_settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mdmv@h2x@dogku^qjcp2)ct6*gk3@l(=#q^*xy(z!0h0^ag&^0'
+# write your secret key here
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
