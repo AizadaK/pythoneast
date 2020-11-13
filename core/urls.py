134591074d58789a46cebe3c9d09e148c5e2ebb2
Aizada <kasenova.aizada@gmail.com>
2020-11-13 12:18:28 +0600

    added registration

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path("", homepage, name="home"),
     path("singin", authorization, name="login"),
+    path("singout/", singout, name="logout"),
+    path("registration/", registration, name="registration")
     
 ]
