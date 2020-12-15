6cf3b9a942d37e33e5c3e3211a6f6fc12ea70a5a
Aizada <kasenova.aizada@gmail.com>
2020-12-15 19:21:04 +0600

    added styles for publications

--- publications/forms.py
+++ publications/forms.py
@@ -3,6 +3,7 @@ from .models import Publication
 
 class PublicationForm(ModelForm):
     name = CharField(max_length=10, help_text='Напишите название публикации')
+
     class Meta:
         model = Publication
         fields = ["name", "text", "image"]
