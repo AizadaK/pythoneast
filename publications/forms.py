769bc170d9fb48d0e855bd036e10445763e032dd
Aizada <kasenova.aizada@gmail.com>
2020-12-22 17:52:44 +0600

    added djanjo-bootstrap and forms

--- publications/forms.py
+++ publications/forms.py
@@ -1,9 +1,10 @@
-from django.forms import ModelForm, CharField
+from django.forms import forms
 from .models import Publication
 
 class PublicationForm(ModelForm):
-    name = CharField(max_length=10, help_text='Напишите название публикации')
-
+    # name = CharField(max_length=10, help_text='Напишите название публикации')
+    # text = forms.TextInput(required=True)
+    # test = forms.CharField(required=True)
     class Meta:
         model = Publication
         fields = ["name", "text", "image"]
